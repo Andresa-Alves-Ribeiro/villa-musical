b5059f5df46cef6110287b3c4391aea3217fde03
Andresa <andresa_15ga@hotmail.com>
2024-02-28 01:46:24 -0300

    personalizando carrousel

--- src/app/components/carousel/carousel.jsx
+++ src/app/components/carousel/carousel.jsx
@@ -22,72 +22,67 @@ export default function CarouselFotos() {
     };
 
     return (
-        <div className='flex flex-row w-4/6 mx-auto bg-zinc-900 justify-between rounded-xl'>
-            <div className='w-2/5 my-auto max-xl:mt-12 pl-4 space-y-4'>
+        <div className='flex flex-row w-4/5 mx-auto bg-zinc-900 border-2 shadow-md shadow-yellow-300 border-yellow-300 border-opacity-30 justify-between rounded-xl'>
+            <div className='w-1/3 my-auto max-xl:mt-12 pl-4 space-y-4'>
                 <button
                     onClick={() => handleItemClick(0)}
-                    className={`flex w-full items-center gap-14 cursor-pointer ${selectedItem === 0 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-300 hover:border-r-4 hover:border-yellow-300'}`}
+                    className={`flex w-full items-center cursor-pointer ${selectedItem === 0 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-200 hover:border-r-4 hover:border-yellow-200'}`}
                 >
                     <Image src={Coral} alt='' width={42} height={40} />
-                    <span>Coral</span>
+                    <span className='flex mx-auto'>Coral</span>
                 </button>
 
                 <button
                     onClick={() => handleItemClick(1)}
-                    className={`flex w-full items-center gap-14 cursor-pointer ${selectedItem === 1 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-300 hover:border-r-4 hover:border-yellow-300'}`}
+                    className={`flex w-full items-center cursor-pointer ${selectedItem === 1 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-200 hover:border-r-4 hover:border-yellow-200'}`}
                 >
                     <Guitar size={40} weight='fill' />
-                    <span>Cordas</span>
+                    <span className='flex mx-auto'>Cordas</span>
                 </button>
 
                 <button
                     onClick={() => handleItemClick(2)}
-                    className={`flex w-full items-center gap-14 cursor-pointer ${selectedItem === 2 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-300 hover:border-r-4 hover:border-yellow-300'}`}
+                    className={`flex w-full items-center cursor-pointer ${selectedItem === 2 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-200 hover:border-r-4 hover:border-yellow-200'}`}
                 >
                     <Image src={Libras2} alt='s' width={36} height={36} />
-                    <span>Libras</span>
+                    <span className='flex mx-auto'>Libras</span>
                 </button>
 
                 <button
                     onClick={() => handleItemClick(3)}
-                    className={`flex w-full items-center gap-14 cursor-pointer ${selectedItem === 3 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-300 hover:border-r-4 hover:border-yellow-300'}`}
+                    className={`flex w-full items-center cursor-pointer ${selectedItem === 3 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-200 hover:border-r-4 hover:border-yellow-200'}`}
                 >
                     <Image src={Saxofone} alt='s' width={36} height={36} />
-                    <span>Saxofone</span>
+                    <span className='flex mx-auto'>Saxofone</span>
                 </button>
 
                 <button
                     onClick={() => handleItemClick(4)}
-                    className={`flex w-full items-center gap-14 cursor-pointer ${selectedItem === 4 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-300 hover:border-r-4 hover:border-yellow-300'}`}
+                    className={`flex w-full items-center cursor-pointer ${selectedItem === 4 ? 'border-r-4 border-yellow-300 text-yellow-300' : 'hover:text-yellow-200 hover:border-r-4 hover:border-yellow-200'}`}
                 >
                     <Image src={Violino2} alt='s' width={40} height={40} />
-                    <span>Violino</span>
+                    <span className='flex mx-auto'>Violino</span>
                 </button>
             </div>
 
             <div className='my-auto h-80 mr-4 border-r-2 border-gray-400' />
 
-            <div className="w-full pl-4">
+            <div className="w-full h-full">
                 <Carousel showThumbs={false} showIndicators={false} selectedItem={selectedItem}>
                     <div>
-                        <Image src={Corais} alt="Slide 1" height={600} width={900} />
-                        <p className="legend">Coral</p>
+                        <Image src={Corais} alt="Slide 1" height={600} width={900} className='rounded-tr-xl rounded-br-xl' />
                     </div>
                     <div>
-                        <Image src={Cordas} alt="Slide 2" height={600} width={900} />
-                        <p className="legend">Cordas</p>
+                        <Image src={Cordas} alt="Slide 2" height={600} width={900} className='rounded-tr-xl rounded-br-xl' />
                     </div>
                     <div>
-                        <Image src={Libras} alt="Slide 3" height={600} width={900} />
-                        <p className="legend">Libras</p>
+                        <Image src={Libras} alt="Slide 3" height={600} width={900} className='rounded-tr-xl rounded-br-xl' />
                     </div>
                     <div>
-                        <Image src={Sax} alt="Slide 4" height={600} width={900} />
-                        <p className="legend">Saxofone</p>
+                        <Image src={Sax} alt="Slide 4" height={600} width={900} className='rounded-tr-xl rounded-br-xl' />
                     </div>
                     <div>
-                        <Image src={Violino} alt="Slide 5" height={600} width={900} />
-                        <p className="legend">Violino</p>
+                        <Image src={Violino} alt="Slide 5" height={600} width={900} className='rounded-tr-xl rounded-br-xl' />
                     </div>
                 </Carousel>
             </div>
